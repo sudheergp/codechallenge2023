8217ff950f6663ef23be8f02767e9774fe105953
subhani465 <68270531+subhani465@users.noreply.github.com>
2023-08-23 14:31:12 +0530

    Add files via upload
    API method2

---
+++ twitter_post_methods.py
@@ -0,0 +1,91 @@
+
+from urllib.parse import parse_qsl
+import requests
+from requests_oauthlib import OAuth1
+import tweepy
+
+
+# Twitter API credentials
+consumer_key = 'c6LSXawfQm3iGSNTqL7LnRYqU'#"consumer_key"
+consumer_secret = '6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9'#"consumer_secret"
+
+# Step 1: Get a request token
+request_token_url = "https://api.twitter.com/oauth/request_token"
+auth = OAuth1(consumer_key, consumer_secret)
+response = requests.post(url=request_token_url, auth=auth)
+request_token_data = dict(parse_qsl(response.text))
+
+# Step 2: Redirect user to authorization URL
+authorize_url = f"https://api.twitter.com/oauth/authorize?oauth_token={request_token_data['oauth_token']}"
+print("Please visit this URL to authorize the app:", authorize_url)
+oauth_verifier = input("Enter the OAuth verifier: ")
+
+# Step 3: Get an access token
+access_token_url = "https://api.twitter.com/oauth/access_token"
+auth = OAuth1(consumer_key, consumer_secret, request_token_data['oauth_token'], request_token_data['oauth_token_secret'])
+response = requests.post(url=access_token_url, auth=auth, params={'oauth_verifier': oauth_verifier})
+access_token_data = dict(parse_qsl(response.text))
+
+# Print the access token and secret (these are your write access credentials)
+print("Access token:", access_token_data['oauth_token'])
+print("Access token secret:", access_token_data['oauth_token_secret'])
+#---------------------------------------------------------
+# Twitter API Tokens
+access_token = access_token_data['oauth_token']#"access_token"
+access_token_secret = access_token_data['oauth_token_secret']#"access_token_secret"
+
+# Create an OAuth 1.0a authentication handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create an API object
+api = tweepy.API(auth)
+
+# Post a tweet
+tweet_text = "#TESTAUTOTHON2023"
+api.update_status(status=tweet_text)
+
+# Make an API call using the authenticated API object
+user = api.get_user(screen_name="sgandlapalli")
+print(user.screen_name)
+print(user.followers_count)
+
+
+
+
+
+
+
+# import oauthlib.oauth1
+# import requests
+#
+# # Your Twitter API credentials
+# consumer_key = 'c6LSXawfQm3iGSNTqL7LnRYqU'#"consumer_key"
+# consumer_secret = '6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9'#"consumer_secret"
+#
+# # Create OAuth 1.0a Client
+# client = oauthlib.oauth1.Client(consumer_key, client_secret=consumer_secret)
+#
+# # Request a temporary token
+# uri, headers, body = client.sign("https://api.twitter.com/oauth/request_token")
+# response = requests.post(uri, headers=headers, data=body)
+# temporary_credentials = oauthlib.oauth1.rfc5849.credentials.Credentials.from_string(response.text)
+#
+# oauthlib.oauth1.rfc5849
+# # Ask the user to authorize the temporary token
+# authorize_url = "https://api.twitter.com/oauth/authorize?oauth_token=" + temporary_credentials["oauth_token"]
+# print("Please visit this URL to authorize the app:", authorize_url)
+# oauth_verifier = input("Enter the OAuth verifier: ")
+#
+# # Exchange the temporary token for an access token
+# token_endpoint = "https://api.twitter.com/oauth/access_token"
+# client = oauthlib.oauth1.Client(consumer_key, client_secret=consumer_secret,
+#                                resource_owner_key=temporary_credentials["oauth_token"],
+#                                resource_owner_secret=temporary_credentials["oauth_token_secret"])
+# uri, headers, body = client.sign(token_endpoint, verifier=oauth_verifier)
+# response = requests.post(uri, headers=headers, data=body)
+# access_credentials = oauthlib.oauth1.rfc5849.credentials.Credentials.from_string(response.text)
+#
+# # Print the access token and secret (these are your write access credentials)
+# print("Access token:", access_credentials["oauth_token"])
+# print("Access token secret:", access_credentials["oauth_token_secret"])
