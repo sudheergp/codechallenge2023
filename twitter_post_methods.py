5013a2757fd8caf0520f83b8171a4f190a501706
subhani465 <68270531+subhani465@users.noreply.github.com>
2023-08-23 15:02:30 +0530

    Update twitter_post_methods.py

--- twitter_post_methods.py
+++ twitter_post_methods.py
@@ -53,39 +53,36 @@ print(user.followers_count)
 
 
 
+#------------------------------------------------------------------------------------------
+
+# c6LSXawfQm3iGSNTqL7LnRYqU - c K
+# 6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9 - c s k
+# 1235133935044014080-FIpuSnyJb1TTQK6gOMxbCp9NteDIYz - access token
+# flM4YOg0G6pxhHDhm2KtubD3j0UXOHISWonY96mJSyygX - access secret token
 
 
+import tweepy
 
-# import oauthlib.oauth1
-# import requests
-#
-# # Your Twitter API credentials
-# consumer_key = 'c6LSXawfQm3iGSNTqL7LnRYqU'#"consumer_key"
-# consumer_secret = '6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9'#"consumer_secret"
-#
-# # Create OAuth 1.0a Client
-# client = oauthlib.oauth1.Client(consumer_key, client_secret=consumer_secret)
-#
-# # Request a temporary token
-# uri, headers, body = client.sign("https://api.twitter.com/oauth/request_token")
-# response = requests.post(uri, headers=headers, data=body)
-# temporary_credentials = oauthlib.oauth1.rfc5849.credentials.Credentials.from_string(response.text)
-#
-# oauthlib.oauth1.rfc5849
-# # Ask the user to authorize the temporary token
-# authorize_url = "https://api.twitter.com/oauth/authorize?oauth_token=" + temporary_credentials["oauth_token"]
-# print("Please visit this URL to authorize the app:", authorize_url)
-# oauth_verifier = input("Enter the OAuth verifier: ")
-#
-# # Exchange the temporary token for an access token
-# token_endpoint = "https://api.twitter.com/oauth/access_token"
-# client = oauthlib.oauth1.Client(consumer_key, client_secret=consumer_secret,
-#                                resource_owner_key=temporary_credentials["oauth_token"],
-#                                resource_owner_secret=temporary_credentials["oauth_token_secret"])
-# uri, headers, body = client.sign(token_endpoint, verifier=oauth_verifier)
-# response = requests.post(uri, headers=headers, data=body)
-# access_credentials = oauthlib.oauth1.rfc5849.credentials.Credentials.from_string(response.text)
-#
-# # Print the access token and secret (these are your write access credentials)
-# print("Access token:", access_credentials["oauth_token"])
-# print("Access token secret:", access_credentials["oauth_token_secret"])
+# Your Twitter API credentials
+consumer_key = 'c6LSXawfQm3iGSNTqL7LnRYqU'#"consumer_key"
+consumer_secret = '6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9'#"consumer_secret"
+access_token = '1694278157556469763-LtLuMpkvyl37BtXIvSow29nHi3uI1u'#"access_token"
+access_token_secret = 'nH14aDIh1LmVrhZNwwuM7lCceDQV6lXLgcryfcc434kro'#"access_token_secret"
+
+# Create an OAuth 1.0a authentication handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create an API object
+api = tweepy.API(auth)
+
+# Post a tweet
+tweet_text = "#TESTAUTOTHON2023"
+api.update_status(status=tweet_text)
+
+# Make an API call using the authenticated API object
+user = api.get_user(screen_name="omprakesh138748")
+print(user.screen_name)
+print(user.followers_count)
+
+
