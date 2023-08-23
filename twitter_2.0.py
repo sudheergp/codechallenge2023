6c2b6e880a83a0bfb28e8056eb9dd4cc35356dd6
subhani465 <68270531+subhani465@users.noreply.github.com>
2023-08-23 16:15:39 +0530

    Update twitter_2.0.py
    api FINAL

--- twitter_2.0.py
+++ twitter_2.0.py
@@ -1,27 +1,35 @@
+import random
+
 import requests
 import json
 
-# Replace with your actual Bearer Token
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAGczpgEAAAAAqOakfljLYdlrPOo47ukLxuzkljs%3Dl8eKEeUB28hkt3gCtaUZZtUw7S8BIuzyZGPjKlcqC5hdxwRg7F"
+random_num = random.randint(100, 9999)
+random_num = str(random_num)
 
-# API endpoint for posting tweets
-tweet_url = "https://api.twitter.com/2/tweets"
 
-# Your tweet content
-tweet_content = "#TESTAUTOTHON2023 - YES WE DID IT ...."
+payload1 = json.dumps({
+    "text": "#STEPINSUMMIT2023 S&P Ratings" + random_num + " @stepin_fourm@veritfy_software"
+})
+
+payload2 = json.dumps({
+    "text": "#TESTAUTOTHON2023 #STEPINSUMMIT2023" + random_num + "@stepin_fourm@veritfy_software"
+})
+
+url = "https://api.twitter.com/2/tweets"
 
 headers = {
-   "Authorization": f"Bearer {bearer_token}",
-   "Content-Type": "application/json"
+  'Content-Type': 'application/json',
+  'Authorization': 'OAuth oauth_consumer_key="JhUC1TwlzzWIbYgZH4huCHt8d",'
+                   'oauth_token="1235133935044014080-GTXl65ApuI5ZfavndAx6phjRLU0uvX",'
+                   'oauth_signature_method="HMAC-SHA1",oauth_timestamp="1692787382",oauth_nonce="vBSMi89sOOX",'
+                   'oauth_version="1.0",oauth_signature="ZXhMflf4vsJrZY4T%2B0iRfzN3d2U%3D"',
+  'Cookie': 'guest_id=v1%3A169276455289286362'
 }
 
-data = {
-   "text": tweet_content
-}
+response1 = requests.request("POST", url, headers=headers, data=payload1)
 
-response = requests.post(tweet_url, headers=headers, data=json.dumps(data))
+response2 = requests.request("POST", url, headers=headers, data=payload2)
 
-if response.status_code == 201:
-   print("Tweet posted successfully!")
-else:
-   print("Error posting tweet:", response.text)
+print(response1.text)
+
+print(response2.text)
