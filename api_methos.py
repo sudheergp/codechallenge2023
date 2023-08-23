d1740bfa2eb1263046770f07fe67d5c4aacb460d
subhani465 <68270531+subhani465@users.noreply.github.com>
2023-08-23 15:11:26 +0530

    Update api_methos.py

--- api_methos.py
+++ api_methos.py
@@ -1,3 +1,41 @@
+
+# c6LSXawfQm3iGSNTqL7LnRYqU - c K
+# 6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9 - c s k
+# 1235133935044014080-FIpuSnyJb1TTQK6gOMxbCp9NteDIYz - access token
+# flM4YOg0G6pxhHDhm2KtubD3j0UXOHISWonY96mJSyygX - access secret token
+
+
+import tweepy
+
+# Your Twitter API credentials
+consumer_key = 'c6LSXawfQm3iGSNTqL7LnRYqU'#"consumer_key"
+consumer_secret = '6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9'#"consumer_secret"
+access_token = '1694278157556469763-LtLuMpkvyl37BtXIvSow29nHi3uI1u'#"access_token"
+access_token_secret = 'nH14aDIh1LmVrhZNwwuM7lCceDQV6lXLgcryfcc434kro'#"access_token_secret"
+
+# Create an OAuth 1.0a authentication handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create an API object
+api = tweepy.API(auth)
+
+# Post a tweet
+tweet_text = "#TESTAUTOTHON2023"
+api.update_status(status=tweet_text)
+
+# Make an API call using the authenticated API object
+user = api.get_user(screen_name="omprakesh138748")
+print(user.screen_name)
+print(user.followers_count)
+
+
+
+
+
+
+
+-------------------------------------------------------------------------------------------------
 import requests
 import json
 import logging
