ed58f0df5348f2115cc4c0fa305480b652b9642c
subhani465 <68270531+subhani465@users.noreply.github.com>
2023-08-23 15:04:18 +0530

    Update twitter_post_methods.py

--- twitter_post_methods.py
+++ twitter_post_methods.py
@@ -6,8 +6,8 @@ import tweepy
 
 
 # Twitter API credentials
-consumer_key = 'c6LSXawfQm3iGSNTqL7LnRYqU'#"consumer_key"
-consumer_secret = '6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9'#"consumer_secret"
+consumer_key = 'gUb5DQ0Sgc5Hm4aYIgcEmc1pU'#"consumer_key"
+consumer_secret = 'TUkwt0sTMIShQ4KsHSt3QgrSIbeGuOs1GLlIe6dH3ggHnSxNww'#"consumer_secret"
 
 # Step 1: Get a request token
 request_token_url = "https://api.twitter.com/oauth/request_token"
@@ -30,59 +30,4 @@ access_token_data = dict(parse_qsl(response.text))
 print("Access token:", access_token_data['oauth_token'])
 print("Access token secret:", access_token_data['oauth_token_secret'])
 #---------------------------------------------------------
-# Twitter API Tokens
-access_token = access_token_data['oauth_token']#"access_token"
-access_token_secret = access_token_data['oauth_token_secret']#"access_token_secret"
-
-# Create an OAuth 1.0a authentication handler
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-# Create an API object
-api = tweepy.API(auth)
-
-# Post a tweet
-tweet_text = "#TESTAUTOTHON2023"
-api.update_status(status=tweet_text)
-
-# Make an API call using the authenticated API object
-user = api.get_user(screen_name="sgandlapalli")
-print(user.screen_name)
-print(user.followers_count)
-
-
-
-
-#------------------------------------------------------------------------------------------
-
-# c6LSXawfQm3iGSNTqL7LnRYqU - c K
-# 6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9 - c s k
-# 1235133935044014080-FIpuSnyJb1TTQK6gOMxbCp9NteDIYz - access token
-# flM4YOg0G6pxhHDhm2KtubD3j0UXOHISWonY96mJSyygX - access secret token
-
-
-import tweepy
-
-# Your Twitter API credentials
-consumer_key = 'c6LSXawfQm3iGSNTqL7LnRYqU'#"consumer_key"
-consumer_secret = '6Z2VT0vXe2SOhZV6o4vPNstWQeo4TrSApyHNyRMhDJOBdbIgg9'#"consumer_secret"
-access_token = '1694278157556469763-LtLuMpkvyl37BtXIvSow29nHi3uI1u'#"access_token"
-access_token_secret = 'nH14aDIh1LmVrhZNwwuM7lCceDQV6lXLgcryfcc434kro'#"access_token_secret"
-
-# Create an OAuth 1.0a authentication handler
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-# Create an API object
-api = tweepy.API(auth)
-
-# Post a tweet
-tweet_text = "#TESTAUTOTHON2023"
-api.update_status(status=tweet_text)
-
-# Make an API call using the authenticated API object
-user = api.get_user(screen_name="omprakesh138748")
-print(user.screen_name)
-print(user.followers_count)
-
 
